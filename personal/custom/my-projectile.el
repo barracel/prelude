3c749b56ce67a624e52297594428fccfa5ead22b
barracel <fernandez.barracel@gmail.com>
2013-06-22 16:21:41 +0200

    Added custom variable to control projectiles project root folder

--- personal/custom/my-projectile.el
+++ personal/custom/my-projectile.el
@@ -1,2 +1,11 @@
 ; projectile settings
 (setq projectile-show-paths-function 'projectile-hashify-with-relative-paths)
+
+;(setq my-projectile-project-root "/home/oso/dev/workspace/jobboard")
+
+;; Custom projectile project root. So I can force it
+(defun projectile-project-root ()
+  (if (boundp 'my-projectile-project-root)
+      my-projectile-project-root
+      (projectile-project-root)))
+
